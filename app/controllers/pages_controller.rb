a41cb52ca42ac2ad45f16be885da3a1d7e18d587
Vitaliy <croaton6@gmail.com>
2016-02-18 01:49:39 +0300

    add blog

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def home
     @categories = Category.limit(4)
     @products = Product.limit(3)
-    @posts = Post.limit(3)
+    @posts = Post.limit(3).decorate
   end
 end
