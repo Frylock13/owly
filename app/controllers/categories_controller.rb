44e289addbfde39b093f7ef4a8b202130c98a6c5
Vitaliy <croaton6@gmail.com>
2016-02-23 19:41:13 +0300

    fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by(slug: params[:slug])
     @products = ProductSorting.call(params[:sort], @category.id)
-    @viewed_products = RecentlyViewedList.call(session[:guest_id])
+    @viewed_products = RecentlyViewedList.call(session[:guest_id]).order('RANDOM()')
   end
 
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,5 +3,5 @@ module ProductsHelper
   def get_viewed_list
     $redis.smembers get_viewed_id
   end
-
+  
 end
