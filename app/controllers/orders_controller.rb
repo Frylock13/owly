ba308cc7bf00800fac6e2d4f64611901e2ef63f1
Vitaliy <croaton6@gmail.com>
2016-02-28 19:55:40 +0300

    order create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,15 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      redirect_to :back
+      flash[:success] = 'Вы успешно оформили заказ'
+    else
+      redirect_to :back
+      flash[:danger] = 'Произошла ошибка'
+    end
   end
 
   private
