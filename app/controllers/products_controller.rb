38e9978a1b3f74e8f6626c8d6cd06facea5cb8b1
Vitaliy <croaton6@gmail.com>
2016-02-29 16:24:57 +0300

    add related products carousel

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,19 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id]).decorate
     @reviews = @product.reviews.decorate
+    @related_products = Product.where(id: related_product_ids)
 
     # Update viewed list(set) in redis
+    update_recently_viewed_list
+  end
+
+  private
+
+  def update_recently_viewed_list
     RecentlyViewedUpdate.call(session[:guest_id], params[:id])
   end
 
+  def related_product_ids
+    @product.related.delete(' ').split(',')
+  end
 end
