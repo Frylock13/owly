61539866a3d2938789d3f73c1f53c9b384456d59
Vitaliy <vitaliy.fry@gmail.com>
2016-03-22 14:42:08 +0300

    fix angular

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,9 +1,10 @@
-var app = angular.module('Owly', ['restangular'])
-  .config(function (RestangularProvider) {
+var app = angular.module('Owly', ['restangular']);
+
+app.config(['RestangularProvider', function(RestangularProvider) {
     
     RestangularProvider.setBaseUrl("http://37.143.11.71/api");
     //RestangularProvider.setRequestSuffix('.json');
     //RestangularProvider.setDefaultHttpFields({xsrfCookieName:'csrftoken', xsrfHeaderName:'X-CSRFToken'});
-});
+}]);
 
 _.contains = _.includes
