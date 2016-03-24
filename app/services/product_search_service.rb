709f60ba637391514f3a93580fda945e0e310d3f
Vitaliy <vitaliy.fry@gmail.com>
2016-03-24 21:50:39 +0300

    fixes

--- app/services/product_search_service.rb
+++ app/services/product_search_service.rb
@@ -1,11 +1,11 @@
 class ProductSearchService
 
   def self.call(sort:, page:, category_id:)
-    products = Product.where(category_id: category_id).page(page).order(:id)
+    products = Product.where(category_id: category_id).page(page)
 
     # If sort param is passed than @instance.sort_method 
     # Or get all products
-    sort ? products.send(sort.to_sym) : products
+    sort ? products.send(sort.to_sym) : products.order(:id)
   end
 
 end
