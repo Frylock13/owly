48c9c4f45b1ad22d4f66faba11d83713bd8e5631
Vitaliy <vitaliy.fry@gmail.com>
2016-04-24 17:06:32 +0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,27 +4,20 @@ class ApplicationController < ActionController::Base
 
   before_action :check_session, :get_categories
 
-  skip_before_filter :verify_authenticity_token
+  before_filter :add_allow_credentials_headers
 
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  def add_allow_credentials_headers                                                                                                                                                                                                                                                        
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5                                                                                                                                                                                                      
+    #                                                                                                                                                                                                                                                                                       
+    # Because we want our front-end to send cookies to allow the API to be authenticated                                                                                                                                                                                                   
+    # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
+    # the browser will not reject the response                                                                                                                                                                                                                                             
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
+    response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
+  end 
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Max-Age'] = '1728000'
-
-      render :text => '', :content_type => 'text/plain'
-    end
+  def options                                                                                                                                                                                                                                                                              
+    head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'                                                                                                                                                                                                         
   end
 
   # Define methods for get cart_id, favorites_id, viewed_id. These components served the redis.
