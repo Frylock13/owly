da752b8ce0cd5f510970034104cd800b4bf1cf1d
Vitaliy <vitaliy.fry@gmail.com>
2016-04-24 16:34:11 +0300

    test cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   before_action :check_session, :get_categories
-
-
-  ### CORS
-  before_filter :add_allow_credentials_headers
-
-  def add_allow_credentials_headers                                                                                                                                                                                                                                                        
-    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5                                                                                                                                                                                                      
-    #                                                                                                                                                                                                                                                                                       
-    # Because we want our front-end to send cookies to allow the API to be authenticated                                                                                                                                                                                                   
-    # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
-    # the browser will not reject the response                                                                                                                                                                                                                                             
-    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
-    response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
-  end 
-
-  def options                                                                                                                                                                                                                                                                              
-    head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'                                                                                                                                                                                                         
-  end
+  skip_before_filter :verify_authenticity_token
 
 
   # Define methods for get cart_id, favorites_id, viewed_id. These components served the redis.
